cf2ad77fbb3e3205e9a1c53aaddb4609165d542f
Kerzzi <kerzzi@outlook.com>
2017-04-18 20:58:28 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
   #get  'static_pages/home'  如果改了首页这个页面还会用到，不要去掉
 end
