ed1b92a05b9d3b0027986e97cb9cb689b6489daf
Kerzzi <kerzzi@outlook.com>
2017-04-18 21:38:11 +0800

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 返回当前登录的用户（如果有的话）
+  # 在持久会话中记住用户
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 返回 cookie 中记忆令牌对应的用户
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)  # 返回当前登录的用户（如果有的话）
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # 如果用户已登录，返回 true，否则返回 false
@@ -15,8 +30,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 忘记持久会话
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # 退出当前用户
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }  #把 email 属性的值转换为小写形式，确保电子邮件地址的唯一性
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #使用正则表达式验证电子邮件地址的格式
@@ -13,6 +14,30 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                 BCrypt::Engine.cost
-                                                BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create(string, cost: cost)
   end
+
+  # 返回一个随机令牌
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 为了持久保存会话，在数据库中记住用户
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+
+  # 如果指定的令牌和摘要匹配，返回 true
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # 忘记用户
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
