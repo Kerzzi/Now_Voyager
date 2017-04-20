c046e103ba74c60b5e1a897e3d2df4f175af3225
Kerzzi <kerzzi@outlook.com>
2017-04-20 18:25:58 +0800

    修复管理员逻辑

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
 
 
   def require_is_admin
-    if current_user.admin?
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
-  
+
   private
 
   # 确保用户已登录
