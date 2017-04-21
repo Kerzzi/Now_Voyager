1f998831a0b9e6b53c7fcdb069b66668a8aeab3e
Kerzzi <kerzzi@outlook.com>
2017-04-21 16:01:22 +0800

    首页优化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,  :following, :followers]
+  before_action :logged_in_user, only: [:edit, :update, :destroy,  :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
