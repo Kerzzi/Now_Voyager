1778bb7814510e29f9317ab331ad9a99c600962a
Kerzzi <kerzzi@outlook.com>
2017-04-23 22:01:09 +0800

    Modify the seed file, automatically generate items

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,50 @@ User.create!(name:  "test2",
                activated_at: Time.zone.now)
 end
 
+
+company_info= [["Apple Inc.","Cupertino, CA, US"] ,["Samsung Electronics","Suwon, South Korea"] ,
+["Amazon.com","Seattle, WA, US"] ,["HP Inc. + HPE","Palo Alto, CA, US"] ,["Microsoft","Redmond, WA, US"] ,
+["IBM","Armonk, NY, US"] ,["Alphabet Inc.","Mountain View, CA, US"] ,["Dell Technologies","Austin, TX, US"] ,
+["Huawei","Shenzhen, China"]]
+
+jobs_info = [["Web Application Developer","Creates, maintains and implements web-based application systems. Resolves issues and recommends enhancements, when necessary. Has knowledge of HTML, Java and related concepts. Relies on knowledge and professional discretion to plan and accomplish goals. Usually reports to a department head. Significant ingenuity and flexibility is expected. May require a bachelor’s degree in a related area and at least 2-4 years of relevant experience."
+],[ "Android Developer","Designs and builds applications for the Android platform. Works with outside data sources and API’s. Fixes bugs and improves application performance. Collaborates with cross-functional teams to determine and launch new features. Should have knowledge of core web technologies (HTML5, CSS3, JavaScript). Requires a bachelor’s degree in area of specialty and 2 years of relevant experience."
+],["iOS Developer","Designs and builds applications for the iOS platform. Works with outside data sources and API’s. Fixes bugs and improves application performance. Collaborates with cross-functional teams to determine and launch new features. Should have knowledge of core web technologies (HTML5, CSS3, JavaScript). Requires a bachelor’s degree in area of specialty and 2 years of relevant experience."
+],["Interface Designer","Create interfaces for a variety of web-based applications. Designs and evaluates visual human interfaces utilizing user-centric design principles. Works with the product development team to achieve desired user experience. Ensures that interfaces function to achieve desired business goals. Relies on established guidelines and instructions to perform daily job functions. Works under immediate supervision and usually reports to a supervisor. May require an associate’s degree with 0-2 years of relevant experience."
+],["Webmaster","Manages an organization’s overall web presence. Monitors web traffic and ensures website is prepared to meet traffic demands and performance expectations. Leads the development and design of the website to enhance appearance and usability. Requires a working knowledge of HTML, JavaScript and SQL. Relies on knowledge and professional discretion to plan and accomplish goals. Works under general supervision and usually reports to a supervisor, though some ingenuity and flexibility is required. May require a bachelor’s degree and 2-4 years of experience."
+]]
+
+
+puts "this seed will create  30 public jobs, and 30 hidden jobs"
+
+create_jobs = for i in 1..30 do
+ job_test=jobs_info[rand(0..4)]
+ company_test=company_info[rand(0..8)]
+ Job.create!([title: job_test[0],
+              description: job_test[1],
+              is_hidden:"true",
+              city: "北京",
+              company: "网易",
+              category: "Developer",
+              user_id: 1])
+end
+
+puts "30 Public jobs created."
+
+create_jobs = for i in 1..30 do
+ job_test=jobs_info[rand(0..4)]
+ company_test=company_info[rand(0..8)]
+ Job.create!([title: job_test[0],
+             description: job_test[1],
+               is_hidden:"true",
+               city: "上海",
+               company: "百度",
+               category: "Developer",
+               user_id: 1])
+end
+
+puts "30 Hidden jobs created."
+
 User.create!(name:  "test3",
             email: "test3@kerzzi.com",
             password:              "123456",
