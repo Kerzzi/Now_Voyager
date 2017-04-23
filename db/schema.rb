1f0b5a7ebe2cd7c920a3115d006f484957cea702
Kerzzi <kerzzi@outlook.com>
2017-04-23 21:38:53 +0800

    add user_id to job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421115951) do
+ActiveRecord::Schema.define(version: 20170423133000) do
 
   create_table "job_relationships", force: :cascade do |t|
     t.integer  "job_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170421115951) do
     t.string   "city"
     t.string   "company"
     t.string   "category"
+    t.integer  "user_id"
   end
 
   create_table "microposts", force: :cascade do |t|
